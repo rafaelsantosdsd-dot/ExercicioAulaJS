79a739e77c0040e983ca1e4acd42c034e7f14b23
Rafael Dantas <rafael.santos.dsd@gmail.com>
2025-10-30 11:32:54 -0300

    Execicios Feitos

---
+++ index.js
@@ -0,0 +1,14 @@
+// exemplo 01 - Operadores Aritmeticos
+
+let x = 4 
+let y = 2
+let z = 3
+let w = -5
+
+console.log("Soma (4+2): "+(x+y))
+console.log("\nSoma com número negativo (4+(-5)): "+(x+w))
+console.log("\nSubtração (4-2): "+(x-y))
+console.log("\nMultiplicação (4*2): "+x*y)
+console.log("\nDivisão (4/2): "+x/y)
+console.log("\nModulo (resto de divisão): "+(x%2))
+console.log("\nModulo (resto de divisão): "+(z%2))
---
+++ index2.js
@@ -0,0 +1,6 @@
+// interpolação de string 
+
+let atividades = 90.5, presenca = 100.00
+
+console.log(`O participante possui ${atividades}% de atividades entregues.`)
+console.log(`\nO O participante possui ${presenca}% de presença.`)
